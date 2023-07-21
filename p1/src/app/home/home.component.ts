5c1b0ee454a4ffaa988262229940bb6d912fa6e7
AnaghaP1999 <100686131+AnaghaP1999@users.noreply.github.com>
2023-07-22 01:28:38 +0530

    requirement crud

--- p1/src/app/home/home.component.ts
+++ p1/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RequirementserviceService } from '../requirementservice.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { RequirementserviceService } from '../requirementservice.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
-  constructor(private reqservice:RequirementserviceService){}
+  itemIdInput: any;
+  constructor(private reqservice:RequirementserviceService, private route: ActivatedRoute, private router:Router){}
 
   items:any;
+  requirements: any = {};
+
 requirementform={
 name:'',
 area:'',
@@ -20,18 +24,42 @@ approved:'0'
 
 }
 
+
 ngOnInit(): void {
   this.reqservice.getRequirements().subscribe((data=>{
     this.items=data;
-    console.log(data);
-    
+    console.log(this.items);
+
   }))
 }
 
-  addrequirement(){
-this.reqservice.requirementadd(this.requirementform).subscribe(res=>{
-alert('Requirement form added successfully');
-})
+// Add a requirement - Admin
+  addrequirement(){console.log('this.requirementform',this.requirementform);
+  
+      this.reqservice.requirementadd(this.requirementform).subscribe(res=>{
+      alert('Requirement form added successfully');
+      window.location.reload();
+    })
   }
 
+  // delete a requirement - Admin
+  deleteRequirement(id: string) {
+    this.reqservice.getDataById(id).subscribe(
+      (response) => {
+        this.requirements = response;
+        if (confirm('Are you sure you want to delete this Requirement?')) { 
+          this.reqservice.deleteRequirement(this.requirements._id).subscribe(
+            () => {
+              console.log('Requirement deleted successfully.');
+              this.router.navigate(['']);
+              window.location.reload();
+            },
+            (error) => {
+              console.error('Error deleting requirement:', error);
+            }
+          );
+        }  
+      }
+    );  
+  }
 }
