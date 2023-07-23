80ec7c62c3dc504fa854fd04cde4ecd1d2bb6b72
mamathaanto <mamathaanto99@gmail.com>
2023-07-23 13:28:04 +0530

    faculty dashboard

--- p1/src/app/app-routing.module.ts
+++ p1/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EditrequirementComponent } from './editrequirement/editrequirement.component';
+import { FacultydashboardComponent } from './facultydashboard/facultydashboard.component';
 
-const routes: Routes = [{path:'',component:HomeComponent},{path:'edit-requirement/:id',component:EditrequirementComponent}];
+const routes: Routes = [{path:'',component:HomeComponent},
+{path:'edit-requirement/:id',component:EditrequirementComponent},
+{path:'facultydashboard',component:FacultydashboardComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
